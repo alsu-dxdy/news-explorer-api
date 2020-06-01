9020416975a919026d520a106c19ff430697f7ba
Alsu Abdrakhmanova <dxdy10@mail.ru>
2020-06-01 16:29:01 +0500

    created m/wares: auth, logger

---
+++ middlewares/auth.js
@@ -0,0 +1,29 @@
+const jwt = require('jsonwebtoken');
+
+const { JWT_SECRET } = require('../config');
+
+const AuthorizationError = require('../errors/AuthorizationError');
+
+const extractBearerToken = (header) => header.replace('Bearer ', '');
+
+// eslint-disable-next-line consistent-return
+module.exports = (req, res, next) => {
+  const { authorization } = req.headers;
+
+  if (!authorization || !authorization.startsWith('Bearer ')) {
+    throw new AuthorizationError('Необходима авторизация');
+  }
+
+  const token = extractBearerToken(authorization);
+  let payload;
+
+  try {
+    payload = jwt.verify(token, JWT_SECRET);
+  } catch (err) {
+    throw new AuthorizationError('Необходима авторизация');
+  }
+
+  req.user = payload; // записываем пейлоуд в объект запроса
+
+  next(); // пропускаем запрос дальше
+};
