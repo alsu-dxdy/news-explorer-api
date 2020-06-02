86cbcb4af494aecf80fe20c89b03131831e0443e
Alsu Abdrakhmanova <dxdy10@mail.ru>
2020-06-02 17:16:10 +0500

    deleted parseInt in  PORT: parseInt

--- config.js
+++ config.js
@@ -1,9 +1,7 @@
 module.exports = {
-  // eslint-disable-next-line radix
-  PORT: parseInt(process.env.PORT) || 3000,
+  PORT: process.env.PORT || 3000,
   DATABASE_URL: 'mongodb://localhost:27017/newexplorerdb',
 
-  // eslint-disable-next-line radix
   JWT_SECRET: process.env.JWT_SECRET || 'JWT_SECRET',
 
 };
