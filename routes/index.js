212045c36b020791aafeba3548df6d63f8b71a7a
Alsu Abdrakhmanova <dxdy10@mail.ru>
2020-06-02 19:14:01 +0500

    deleted commented code from routes/index.js

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,9 @@
 const router = require('express').Router();
 const { celebrate } = require('celebrate');
-// const users = require('./users');
-// const articles = require('./articles');
+
 const auth = require('../middlewares/auth');
 const { signUpSchema } = require('../schemasJoi/signup');
 const { signInSchema } = require('../schemasJoi/signin');
-// const createUser = require('../controllers/users');
-// const login = require('../controllers/users');
 
 const { createUser, login } = require('../controllers/users');
 
@@ -22,17 +19,5 @@ router.use('/articles', require('./articles'));
 router.use((req, res) => {
   res.status(404).send({ message: 'Запрашиваемый ресурс не найден' });
 });
-
-
-// router.post('/signUp', celebrate(schema), auth.signUp);
-// router.post('/signIn', auth.signIn);
-
-// router.use('/auth', require('./auth'));
-
-// router.use('/cards', auth.authenticate, cards);
-// router.use('/users', users);
-// router.use('*', (req, res, next) => {
-//   next({ status: 404, message: 'route not found' })
-// });
 
 module.exports = router;
