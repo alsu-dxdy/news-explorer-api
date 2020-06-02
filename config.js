387368a1d779d0b75c9051e45ab322a6c5f144ed
Alsu Abdrakhmanova <dxdy10@mail.ru>
2020-06-02 19:01:38 +0500

    added radix 10 in PORT: parseInt

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  PORT: process.env.PORT || 3000,
+  PORT: parseInt(process.env.PORT, 10) || 3000,
   DATABASE_URL: 'mongodb://localhost:27017/newexplorerdb',
 
   JWT_SECRET: process.env.JWT_SECRET || 'JWT_SECRET',
