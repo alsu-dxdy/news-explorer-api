9b74fdc231941a04b1c431ff2f6a45098a61282a
Alsu Abdrakhmanova <dxdy10@mail.ru>
2020-06-04 19:22:34 +0500

    edited config: DATABASE_URL: process.env.DATABASE_URL

--- config.js
+++ config.js
@@ -1,7 +1,5 @@
 module.exports = {
-  PORT: parseInt(process.env.PORT, 10) || 3000,
-  DATABASE_URL: 'mongodb://localhost:27017/newexplorerdb',
-
   JWT_SECRET: process.env.JWT_SECRET || 'JWT_SECRET',
-
+  PORT: parseInt(process.env.PORT, 10) || 3000,
+  DATABASE_URL: process.env.DATABASE_URL || 'mongodb://localhost:27017/newexplorerdb',
 };
