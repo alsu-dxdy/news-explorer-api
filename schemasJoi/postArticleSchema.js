ea922e92aa8ff66dcd3128b50f1225c521211093
Alsu Abdrakhmanova <dxdy10@mail.ru>
2020-06-30 19:16:40 +0500

    Joi article: deleted regex in link

--- schemasJoi/postArticleSchema.js
+++ schemasJoi/postArticleSchema.js
@@ -19,7 +19,6 @@ module.exports.postArticleSchema = {
       .min(2)
       .required(),
     link: Joi.string()
-      .regex(/^http(s)?:\/\/(www\.)?(?!(www\.))(([0-2][0-5][0-5]\.){3}[0-2][0-5][0-5]|([a-z0-9]{2,}[-a-z0-9]*\.)+[a-z]{2,6})(\/)?(:\d{2,5})?([-?=&A-Za-z0-9.]{2,}\/?)*#?\d?$/)
       .required(),
     image: Joi.string()
       .required(),
